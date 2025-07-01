ac3d557c09836c60c28dd09179ef8b8d934b6c33
alecarnun <alecarnun@alum.us.es>
2025-07-01 18:40:54 +0200

    feature: Clase java para el problema QUBO. Método fitness

--- src/main/java/es/uma/informatica/misia/ae/simpleea/QUBOProblem.java
+++ src/main/java/es/uma/informatica/misia/ae/simpleea/QUBOProblem.java
@@ -8,4 +8,16 @@ public class QUBOProblem {
         this.Q = Q;
         this.n = Q.length;
     }
+    public double fitness(int[] solution) {
+        if (solution.length != n) {
+            throw new IllegalArgumentException("The size of the solution must be equal to the length of the QUBO problem.");
+        }
+        double result = 0.0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result += Q[i][j] * solution[i] * solution[j];
+            }
+        }
+        return result;
+    }
 }
